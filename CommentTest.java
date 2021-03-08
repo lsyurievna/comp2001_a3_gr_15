f6da44a6e23864c698bcccc3e76f4a55d6492ea0
lsyurievna <lsyurievna@mun.ca>
2021-03-08 18:05:07 +0300

    Errors hopefully fixed for the Comment class.

--- CommentTest.java
+++ CommentTest.java
@@ -15,11 +15,11 @@ import org.junit.Test;
  */
 public class CommentTest
 {
-    private Comment comment0;
+    
     private Comment comment1;
     private Comment comment3;
     private Comment comment5;
-    private Comment comment6;
+    
 
     /**
      * Default constructor for test class CommentTest
@@ -36,11 +36,18 @@ public class CommentTest
     @Before
     public void setUp()
     {
-        comment0 = new Comment("Tom Hanks", "Worse thing I've ever seen", 0);
+        
         comment1 = new Comment("Lila Doomsday", "Not recommending this to anyone", 1);
+        
         comment3 = new Comment("user2021", "Kinda bad but I like the color", 3);
+        comment3.upvote();
+        
         comment5 = new Comment("Jenna Williams", "Best gift for your ex", 5);
-        comment6 = new Comment("Bob Dumb", "Best present I ever received!!!", 6);
+        comment5.upvote();
+        comment5.upvote();
+        
+        
+       
     }
 
     /**
@@ -56,26 +63,24 @@ public class CommentTest
     @Test
     public void test_getRating()
     {
-        assertEquals(0, comment0.getRating());
+        
         assertEquals(1, comment1.getRating());
         assertEquals(3, comment3.getRating());
         assertEquals(5, comment5.getRating());
-        assertEquals(6, comment6.getRating());
+        
     }
 
     @Test
     public void test_downVote()
     {
-        comment0.downvote();
-        assertEquals(1, comment0.getRating());
+       
         comment1.downvote();
-        assertEquals(1, comment1.getRating());
+        assertEquals(0, comment1.getVoteCount()); // 0 is the minimum
         comment3.downvote();
-        assertEquals(2, comment3.getRating());
+        assertEquals(1, comment3.getVoteCount());
         comment5.downvote();
-        assertEquals(4, comment5.getRating());
-        comment6.downvote();
-        assertEquals(5, comment6.getRating());
+        assertEquals(2, comment5.getVoteCount());
+       
     }
 }
 
